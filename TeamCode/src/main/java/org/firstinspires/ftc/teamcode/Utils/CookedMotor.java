6cd19f15dca59dd0a773b5ff8e0284f60b4d9a12
3DCoded <info3dcoded@gmail.com>
2025-11-20 10:36:16 -0500

    fix: Add a grace period to CookedMotor so it doesn't interfere with launcher during initial spinup.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/CookedMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/CookedMotor.java
@@ -1,24 +1,37 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Utils;
 
+import android.os.health.SystemHealthManager;
+
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
-import org.firstinspires.ftc.robotserver.internal.webserver.RobotControllerWebHandlers;
-import org.firstinspires.ftc.teamcode.Utils.EnhancedMotor;
 
+@Config
 public class CookedMotor {
+    /*
+    Motor overcurrent protection
+    When the motor exceeds maxCurrent (typical GoBilda 5202/3/4 series are rated to 9A),
+    the motor will briefly stop and the linked gamepad will rumble.
+     */
     private DcMotorEx motor;
     private double maxCurrent = 5.0; // Amps
+
+    private long lastNotCookedTime;
+    public static double COOK_DURATION = 1000;
 
     public CookedMotor(DcMotorEx motor, double maxCurrent) {
         this.motor = motor;
         this.maxCurrent = maxCurrent;
+        lastNotCookedTime = System.currentTimeMillis();
     }
 
     public boolean isCooked() {
-        return motor.getCurrent(CurrentUnit.AMPS) > maxCurrent;
+        boolean cooked = motor.getCurrent(CurrentUnit.AMPS) > maxCurrent;
+        if (!cooked) { lastNotCookedTime = System.currentTimeMillis(); }
+        return cooked;
     }
 
     public void loop() {
@@ -26,7 +39,7 @@ public class CookedMotor {
     }
 
     public void loop(Gamepad gamepad) {
-        if (isCooked()) {
+        if (isCooked() && System.currentTimeMillis() - lastNotCookedTime > COOK_DURATION) {
             motor.setPower(0.0);
             motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             if (!gamepad.isRumbling()) {
